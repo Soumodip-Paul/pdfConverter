78f8feb9af9da4485bb86c5578d0d240fa8113c9
Soumodip Paul <soumodippaul10032002@outlook.in>
2023-03-05 18:03:01 +0530

    kk

--- pdfConverter/pdfConverter/settings.py
+++ pdfConverter/pdfConverter/settings.py
@@ -134,8 +134,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/assets/'
-STATIC_ROOT = BASE_DIR / 'assets'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
